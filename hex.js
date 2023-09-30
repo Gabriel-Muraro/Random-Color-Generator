416729cf05cddbc632a0284e0e1fdaa88ade0434
Gabriel Muraro <132502562+Gabriel-Muraro@users.noreply.github.com>
2023-09-30 15:50:43 -0300

    Finished coding the "hex" part of the color randomizer
    Made some changes in JS on the "hex" one version, that is the true color randomizer, once the simple version its just a random picker between the values already defined.
    Added coments so i can have more knowledge about what i am doing for each part from the code.

--- hex.js
+++ hex.js
@@ -1,19 +1,31 @@
+// MADE A VARIABLE CONST WITH 16 VALUES THAT WILL BE OUR HEXADECIMAL NUMBER
 const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
 
-const btn = document.getElementById('btn')
+//GET THE ID'S FROM DE HTML CODE TO A VARIABLE CONSTANT
+const btn = document.getElementById("btn");
 const color = document.querySelector(".color");
 
-btn.addEventListener('click', function(){
+// FUNCTION ON CLICK
+btn.addEventListener('click', function () {
 
+    //ADDING THE "#" BEFORE THE COLOR TO VS RECOGNIZE THEM
     let hexColor = '#';
 
-    for(let i = 0; i<6; i++){
+    //USING FOR TO GET 6 RANDOM NUMBERS
+    for (let i = 0; i < 6; i++) {
 
-        hexColor += hex[0]
+        hexColor += hex[getRandomNumber()];
 
     }
 
+    //SHOWING THE COLOR NAME AND CHANGING THE BACKGROUND 
     color.textContent = hexColor;
     document.body.style.backgroundColor = hexColor;
 
-})
+});
+
+//MADE A FUNCTION TO GET A RANDOM NUMBER BASED ON THE LENGTH THAT IS A HEXADECIMAL NUMBER WITH 6 CHARACTERS
+function getRandomNumber() {
+    return Math.floor(Math.random() * hex.length);
+
+}
